1a025bac7a42e58f954b321f1e2a1066037f37ec
felixerus <felixerus@gmail.com>
2017-11-27 19:47:28 -0800

    Add Card values and Suits

---
+++ src/main/java/org/airbrontosaurus/john/model/CardValue.java
@@ -0,0 +1,41 @@
+package org.airbrontosaurus.john.model;
+
+public enum  CardValue {
+
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+
+    @Override
+    public String toString() {
+
+        switch (this) {
+            case ACE:
+                return "A";
+            case TWO:
+                return "2";
+            case THREE:
+                return "3";
+            case FOUR:
+                return "4";
+            case FIVE:
+                return "5";
+            case SIX:
+                return "6";
+            case SEVEN:
+                return "7";
+            case EIGHT:
+                return "8";
+            case NINE:
+                return "9";
+            case TEN:
+                return "10";
+            case JACK:
+                return "J";
+            case QUEEN:
+                return "Q";
+            case KING:
+                return "J";
+            default:
+                    return "Joker";
+    }
+}
+}
---
+++ src/main/java/org/airbrontosaurus/john/model/Suit.java
@@ -0,0 +1,23 @@
+package org.airbrontosaurus.john.model;
+
+public enum Suit {
+
+    SPADE, CLUB, DIAMOND, HEART;
+
+    @Override
+    public String toString() {
+
+        switch (this) {
+            case SPADE:
+                return "Spade";
+            case CLUB:
+                return "Club";
+            case DIAMOND:
+                return "Diamond";
+            case HEART:
+                return "Heart";
+            default:
+                return "Joker";
+        }
+    }
+}
